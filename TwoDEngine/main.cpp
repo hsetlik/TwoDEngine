49d9676423b129389620f34b8c3d1b26f1504129
hayden setlik <haydensetlik@gmail.com>
2020-07-31 22:07:47 -0600

    Initial Commit

---
+++ TwoDEngine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TwoDEngine
+//
+//  Created by Hayden on 7/31/20.
+//  Copyright © 2020 Hayden. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
